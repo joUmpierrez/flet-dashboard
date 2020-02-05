43d5878358de701d49287e3a4b8de7afb25cc1fa
Pablo Diaz <pablo.diaz2799@gmail.com>
2020-02-04 22:19:52 -0300

    added ngxChart

--- dashboard/src/app/pages/order-stats-page/order-stats.component.ts
+++ dashboard/src/app/pages/order-stats-page/order-stats.component.ts
@@ -5,89 +5,115 @@ import { OrderService } from 'app/shared/services/orders/order.service';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from 'app/shared/auth/auth.service';
 import {
-    IBarChartOptions,
-    IChartistAnimationOptions,
-    IChartistData
+  IBarChartOptions,
+  IChartistAnimationOptions,
+  IChartistData
   } from 'chartist';
   import { ChartEvent, ChartType } from 'ng-chartist';
 
 @Component({
-    // tslint:disable-next-line: component-selector
-    selector: 'order-stats',
-    templateUrl: './order-stats-component.html',
-    styleUrls: ['./order-stats.component.scss']
+  // tslint:disable-next-line: component-selector
+  selector: 'order-stats',
+  templateUrl: './order-stats-component.html',
+  styleUrls: ['./order-stats.component.scss']
 })
 
 export class OrderStatsComponent implements OnInit {
 
-    pieChartData = [200, 100, 500];
-    pieChartLabels = ['Set 1', 'Set 2', 'Set 3'];
-    pieChartType = 'pie';
-    pieChartOptions = {
-      animation: true,
-      responsive: true,
-      mainAspectRatio: false,
-    }
+  pieChartData = [200, 100, 500];
+  pieChartLabels = ['Set 1', 'Set 2', 'Set 3'];
+  pieChartType = 'pie';
+  pieChartOptions = {
+    animation: true,
+    responsive: true,
+    mainAspectRatio: false,
+  }
 
-    type: ChartType = 'Line';
-    data: IChartistData = {
-      labels: [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec'
-      ],
-      series: [
-        [30, 50, 70, 80, 60, 20, 40, 10, 60, 90, 100, 70]
-      ]
-    };
-
-    options: IBarChartOptions = {
-      axisX: {
-        showGrid: false
-      },
-      axisY: {
-          showGrid: true,
-      },
-      height: 300
-    };
-
-    events: ChartEvent = {
-      draw: (data) => {
-        if (data.type === 'line') {
-          data.element.animate({
-            y2: <IChartistAnimationOptions>{
-              dur: '0.5s',
-              from: data.y1,
-              to: data.y2,
-              easing: 'easeOutQuad'
-            }
-          });
-        }
+  barChartType: ChartType = 'Bar';
+  barChartData: IChartistData = {
+    series: [[49, 45, 30, 30, 30]],
+    labels: ['13hs', '12hs', '11hs', '14hs', '15hs']
+  }
+  barChartOptions: IBarChartOptions = {
+    axisX: {
+      showGrid: false
+    },
+    height: 300
+  };
+  barChartEvents: ChartEvent = {
+    draw: (data) => {
+      if (data.type === 'bar') {
+        data.element.animate({
+          y2: <IChartistAnimationOptions>{
+            dur: '0.5s',
+            from: data.y1,
+            to: data.y2,
+            easing: 'easeOutQuad'
+          }
+        });
       }
-    };
-
-    // private orderService: OrderService
-    // public orders: any;
-    // public total_orders: any;
-    // public events: any;
-    // public dt_columns: any;
-    // @ViewChild('action_column', { static: true }) action_column: TemplateRef<any>;
-
-    constructor(private http: HttpClient, private route: Router, private auth: AuthService) {
-        // this.orderSerice = new OrderService();
-        // this.orderService = new OrderService(http,auth);
     }
+  };
 
-    ngOnInit(): void {
+  type: ChartType = 'Line';
+  data: IChartistData = {
+    labels: [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ],
+    series: [
+      [20, 25, 20, 30, 45, 49, 30, 30, 25, 23, 25, 15]
+    ]
+  };
 
+  options: IBarChartOptions = {
+    axisX: {
+      showGrid: false
+    },
+    axisY: {
+        showGrid: true,
+    },
+    height: 300
+  };
+
+  events: ChartEvent = {
+    draw: (data) => {
+      if (data.type === 'line') {
+        data.element.animate({
+          y2: <IChartistAnimationOptions>{
+            dur: '0.5s',
+            from: data.y1,
+            to: data.y2,
+            easing: 'easeOutQuad'
+          }
+        });
+      }
     }
+  };
+
+  // private orderService: OrderService
+  // public orders: any;
+  // public total_orders: any;
+  // public events: any;
+  // public dt_columns: any;
+  // @ViewChild('action_column', { static: true }) action_column: TemplateRef<any>;
+
+  constructor(private http: HttpClient, private route: Router, private auth: AuthService) {
+      // this.orderSerice = new OrderService();
+      // this.orderService = new OrderService(http,auth);
+  }
+
+  ngOnInit(): void {
+
+  }
 }
